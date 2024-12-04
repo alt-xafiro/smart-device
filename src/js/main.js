1c775e46a63734f6c1014543e928bbf6e9f61117
alt-safiro <altxafiro@gmail.com>
2024-12-04 19:02:22 +0300

    Upload the repo to GitHub

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+import { enableJs } from './util.js';
+import 'focus-visible';
+import { setAccordionsListeners } from './accordion.js';
+import {
+  setPromoOrderButtonListeners,
+  setPromoOrderButtonText,
+} from './promo.js';
+import {
+  replaceOpenModalLinksWithButtons,
+  setModalListeners,
+} from './modal.js';
+import { setWriteUsListeners } from './validation.js';
+
+enableJs();
+
+setPromoOrderButtonText();
+setPromoOrderButtonListeners();
+
+setAccordionsListeners();
+
+replaceOpenModalLinksWithButtons();
+setModalListeners();
+
+setWriteUsListeners();
